06d26f1cfcda9f9f32825d1c0d341cc927bf2c50
Margaret Farron <73440342+mpnauss@users.noreply.github.com>
2021-05-25 12:16:27 -0700

    Update routes on home controller

--- src/main/java/com/launchacademy/reviews/controllers/HomeController.java
+++ src/main/java/com/launchacademy/reviews/controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-  @GetMapping(value = {"/", "/category", "/category/restaurant", "/category/restaurant/:id", "/restaurants", "/restaurant/new", "review/new"})
+  @GetMapping(value = {"/", "/categories", "/{categoryName}/restaurant/{id}", "/{categoryName}/restaurants", "/restaurant/new", "review/new"})
   public String forward() {
     return "forward:/index.html";
   }
