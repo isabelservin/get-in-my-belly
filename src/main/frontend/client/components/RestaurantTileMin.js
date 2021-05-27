5e59108d46201f8faac4aa9141a4c7ba88f3763f
Hai-Au Bui <haiaunht@yahoo.com>
2021-05-27 14:34:23 -0700

    solved all the conflicts, reviews are now listing

--- src/main/frontend/client/components/RestaurantTileMin.js
+++ src/main/frontend/client/components/RestaurantTileMin.js
@@ -1,9 +1,20 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import ReviewTile from "./ReviewTile";
 
 const RestaurantTileMin = props => {
   console.log(props.imgUrl)
-
+  const showListOfReview = props.restaurant.reviews.map( review => {
+    return (
+        <ReviewTile
+            key={review.id}
+            id={review.id}
+            datePosted={review.datePosted}
+            review={review.review}
+            rating={review.restaurantRating}
+        />
+    )
+  })
 
   return (
     <div className="cell">
@@ -11,13 +22,14 @@ const RestaurantTileMin = props => {
         <div className="card-divider">
           <h2><Link to={`/${props.type}/restaurant/${props.id}`}>{props.name}</Link></h2>
         </div>
-        <Link to={`${props.type}/restaurant/${props.id}`}>
+        <Link to={`/${props.type}/restaurant/${props.id}`}>
           <img className="images thumbnail" src={props.imgUrl} />
         </Link>
         <div className="card-section">
           <p>
-            <strong>Review:</strong> {props.review} <br/>
-            <button className="review-button" href="/review/new" ><Link to="/review/new">Write a Review</Link></button>
+            <strong>Review:</strong>
+            {showListOfReview} <br/>
+            <button className="review-button" href="/review/new" ><Link to={`/${props.id}/review/new`}>Write a Review</Link></button>
           </p>
         </div>
       </div>
