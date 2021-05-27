98bc47b7405822ccf91eb107733631556e5c86fe
a-moairu <61666763+a-moairu@users.noreply.github.com>
2021-05-27 12:00:42 -0700

    fixed form

--- src/main/java/com/launchacademy/reviews/services/ReviewService.java
+++ src/main/java/com/launchacademy/reviews/services/ReviewService.java
@@ -1,8 +1,6 @@
 package com.launchacademy.reviews.services;
 
-import com.launchacademy.reviews.models.Location;
 import com.launchacademy.reviews.models.Review;
-import com.launchacademy.reviews.repositories.LocationRepository;
 import com.launchacademy.reviews.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,12 +10,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReviewService {
   private ReviewRepository reviewRepository;
-  private LocationRepository locationRepository;
 
   @Autowired
-  public ReviewService(ReviewRepository reviewRepository, LocationRepository locationRepository) {
+  public ReviewService(ReviewRepository reviewRepository) {
     this.reviewRepository = reviewRepository;
-    this.locationRepository = locationRepository;
   }
 
   public Page<Review> findAll(Pageable pageable) {
